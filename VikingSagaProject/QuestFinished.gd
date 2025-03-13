bd7c209ed96efc23f9687807adf6660ba04d3d35
Buffalo Bira <bjonke@users.noreply.github.com>
2025-03-13 01:50:50 +0100

    Revert "Update QuestFinished.gd"

--- VikingSagaProject/QuestFinished.gd
+++ VikingSagaProject/QuestFinished.gd
@@ -1,41 +1,22 @@
 extends Control
-class_name QuestFinished
 
-signal quest_continued
-
-@export var fade_duration: float = 0.5
-@export var quest_name: String = "Unnamed Quest"
-
-@onready var title_label = $Panel/VBoxContainer/Title
-@onready var desc_label = $Panel/VBoxContainer/Description
-@onready var audio_player = $AudioStreamPlayer
-
+@onready var globals = get_node("/root/Globals")
+# Called when the node enters the scene tree for the first time.
 func _ready():
-    # Update UI with quest information
-    title_label.text = "%s Completed!" % quest_name
-    desc_label.text = "Congratulations on finishing %s!" % quest_name
-    
-    # Fade in animation
-    modulate.a = 0.0
-    var tween = create_tween()
-    tween.tween_property(self, "modulate:a", 1.0, fade_duration).set_ease(Tween.EASE_IN_OUT)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
 
 func _on_button_pressed():
-    # Fade out animation before continuing
-    var tween = create_tween()
-    tween.tween_property(self, "modulate:a", 0.0, fade_duration).set_ease(Tween.EASE_IN_OUT)
-    tween.tween_callback(_emit_continue_signal)
-    
-    if audio_player:
-        audio_player.stop()
-
-func _emit_continue_signal():
-    quest_continued.emit()
-    queue_free()
-
-func set_quest_info(name: String, description: String = ""):
-    quest_name = name
-    if description:
-        desc_label.text = description
-    if is_node_ready():
-        title_label.text = "%s Completed!" % quest_name
+	hide()
+	print("pressing the matter")
+	globals.QuestWater = 0
+	globals.QuestFood = 0
+	globals.QuestTrees = 0
+	globals.QuestClay = 0
+	globals.QuestHunting = 0
+	pass # Replace with function body.
